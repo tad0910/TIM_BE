e1fd95ae344eafb705af4d4818d64ea4f3c9f1e3
Lehaibac05 <lehbac05@gmail.com>
2025-09-29 11:32:41 +0700

    update latest 29/9

---
+++ src/main/java/com/tim/appTim/appApplication/AppTimApplication.java
@@ -0,0 +1,13 @@
+package com.tim.appTim.appApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppTimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppTimApplication.class, args);
+	}
+
+}
