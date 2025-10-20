f2c59c9d740f46700678998696ecf2d925667d8a
Lehaibac05 <lehbac05@gmail.com>
2025-10-20 13:45:51 +0700

    test push

--- src/main/java/com/tim/appTim/config/CustomJwtAuthenticationProvider.java
+++ src/main/java/com/tim/appTim/config/CustomJwtAuthenticationProvider.java
@@ -13,7 +13,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class CustomJwtAuthenticationProvider implements AuthenticationProvider {
 
     private final JwtUtil jwtUtil;
