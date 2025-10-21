2bc2fe329d99aa342f87fafaf0be7e482d73cbf5
Lehaibac05 <bac.le@software.codegym.vn>
2025-10-21 09:57:36 +0700

    update class api

--- src/main/java/com/tim/appTim/repository/ClassMemberRepository.java
+++ src/main/java/com/tim/appTim/repository/ClassMemberRepository.java
@@ -1,6 +1,7 @@
 package com.tim.appTim.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,4 +11,6 @@ import com.tim.appTim.entity.ClassMember;
 @Repository
 public interface ClassMemberRepository extends JpaRepository<ClassMember, Long> {
     List<ClassMember> findByClassId(Long classId);
+    List<ClassMember> findByUserId(Long userId);
+    Optional<ClassMember> findByClassIdAndUserId(Long classId, Long userId);
 }
