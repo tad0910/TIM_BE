e59e31e8daadccae1bef17891a5da7adb5d0fa8f
duongdai184 <duong.nguyen@software.codegym.vn>
2025-11-27 23:25:11 +0700

    update

--- src/main/java/com/tim/appTim/controller/ReceiptController.java
+++ src/main/java/com/tim/appTim/controller/ReceiptController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
@@ -21,6 +22,7 @@ public class ReceiptController {
         private com.tim.appTim.service.TuitionTransactionService tuitionTransactionService;
 
         @GetMapping("/{id}/download")
+        @Transactional(readOnly = true)
         public ResponseEntity<byte[]> downloadReceipt(
                         @PathVariable Long id,
                         @RequestParam(required = false) String name,
